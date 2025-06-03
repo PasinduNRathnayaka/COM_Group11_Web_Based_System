e30c05b4d8b9c150f1ca7cf78f07892d8705809b
Pasindu Niroshan Rathnayaka <rathnayaka12106@usci.ruh.ac.lk>
2025-06-04 00:17:57 +0530

    create AddProduct.js and AddProduct.css in src/components.
    Edit App.js

--- client/owner-portal/src/App.js
+++ client/owner-portal/src/App.js
@@ -2,12 +2,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import AddProduct from "./components/AddProduct";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Sidebar />
+        <div className="content">
+          <Routes>
+            <Route path="/add-product" element={<AddProduct />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
