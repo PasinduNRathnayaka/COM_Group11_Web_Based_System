d39a15559d2523d1b49746796fb6bdee9cc5bbaf
Pasindu Niroshan Rathnayaka <rathnayaka12106@usci.ruh.ac.lk>
2025-06-19 13:36:05 +0530

    PQR6004 - Update App.jsx for SellerLogin and SellerLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { useAppContext } from './context/AppContext';
+import { Toaster } from 'react-hot-toast';
+import SellerLogin from './pages/seller/SellerLogin';
+import SellerLayout from './pages/seller/SellerLayout'; // ✅ import SellerLayout
 
 const App = () => {
-    return (
-      <div>
-          <Navbar/>
-      </div>
-    )
-}
+  const isSellerPath = useLocation().pathname.includes("seller");
+  const { isSeller } = useAppContext();
 
-export default App
+  return (
+    <div>
+      {!isSellerPath && <Navbar />}
+      <Toaster />
+
+      <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+        <Routes>
+          <Route path="/seller" element={isSeller ? <SellerLayout /> : <SellerLogin />} /> {/* Show SellerLogin only when not logged in */}
+        </Routes>
+      </div>
+
+      {!isSellerPath && <footer />}
+    </div>
+  );
+};
+
+export default App;
