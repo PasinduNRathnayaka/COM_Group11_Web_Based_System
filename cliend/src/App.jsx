ef4816f649c4c4cb7920ddca99beee55e4434410
Pasindu-Indusara23 <pasinduindusara9@gmail.com>
2025-06-24 11:34:19 +0530

    IMG1002- App.jsx Updated

--- cliend/src/App.jsx
+++ cliend/src/App.jsx
@@ -5,6 +5,10 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import { useAppContext } from './context/AppContext';
 import { Toaster } from 'react-hot-toast';
 
+import Home from './pages/User/Home' //new
+import LoginModal from './components/LoginModel' //new
+import SignUp from './pages/User/SignUp' //new
+
 import SellerLogin from './pages/seller/SellerLogin';
 import SellerLayout from './pages/seller/SellerLayout';
 
@@ -18,13 +22,35 @@ import MarkAttendance from './pages/seller/MarkAttendence';
 const App = () => {
   const location = useLocation();
   const isSellerPath = location.pathname.startsWith("/seller");
-  const { isSeller, showUserLogin } = useAppContext();
+  const { isSeller, showUserLogin, navigate, setShowUserLogin } = useAppContext(); //new
+//new
+const handleSignInClick = () => {     
+    setShowUserLogin(false)  // Close login modal
+    navigate('/signup')      // Redirect to /signup page
+  }
+//new
 
   return (
     <div className='text-default min-h-screen text-gray-700 bg-white'>
-      {!isSellerPath && <Navbar />}
-      {/* Optional login modal */}
-      {showUserLogin && <Login />} 
+      {/* {!isSellerPath && <Navbar />} */}
+     {/* Optional login modal */}
+      {/* {showUserLogin && <Login />}  */}
+{/* new  */}
+     {isSellerPath ? null : <Navbar />}
+      <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<SignUp />} /> 
+        </Routes>
+      </div>
+{/* new  */}
+
+     {/* Render login modal */}
+      <LoginModal isOpen={showUserLogin} onClose={() => setShowUserLogin(false)} 
+
+      onSignInClick={handleSignInClick}
+      />
+{/* new  */}
       <Toaster />
 
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
