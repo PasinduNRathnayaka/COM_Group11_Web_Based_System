8db47d056aaa388cb702878fd71050b05543e074
Pasindu Niroshan Rathnayaka <rathnayaka12106@usci.ruh.ac.lk>
2025-07-09 23:59:37 +0530

    PQR1104 - create dummy attendence for emloyee

---
+++ cliend/src/pages/Employee/Attendance.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Attendance = () => {
+  return (
+    <div className="text-center mt-20 text-xl font-semibold">
+      This is Add Employee area
+    </div>
+  );
+};
+
+export default Attendance;
