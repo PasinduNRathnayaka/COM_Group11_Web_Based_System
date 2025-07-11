ce19963b762582f8dc06c628d19be137a670a99a
TharuApk99 <tharushirajapaksha11@gmail.com>
2025-07-11 10:51:00 +0530

    THA0102 create viewattendance side bar

---
+++ cliend/src/components/Employee/ViewAttendanceNavbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ViewAttendanceNavbar = () => {
+  return (
+    <aside className="w-64 bg-blue-100 p-6 min-h-screen text-sm shadow-md">
+      <h1 className="text-2xl font-bold mb-10 text-blue-900">profile</h1>
+    <ul className="space-y-4 text-blue-900 font-medium">
+         <h1 className="text-2xl font-bold mb-10 text-blue-900">ATTENDANCE</h1>
+    </ul>
+    </aside>
+  );
+};
+
+export default ViewAttendanceNavbar;
+
