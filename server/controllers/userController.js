c3e74fc985f548d84b49873243d0e619ed7ab897
Pasindu-Indusara23 <pasinduindusara9@gmail.com>
2025-07-15 23:44:10 +0530

    IMG5005-usercontroller.jsx updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,11 @@
 // User-specific actions
+import asyncHandler from 'express-async-handler';
+import User from '../models/User.js';
+import generateToken from '../utils/generateToken.js';
+import bcrypt from 'bcryptjs';
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+
+  });
