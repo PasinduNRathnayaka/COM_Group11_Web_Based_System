dd6b9f130ea42c1d81e30fc24325f2e4794cfc6d
Pasindu-Indusara23 <pasinduindusara9@gmail.com>
2025-07-16 02:19:10 +0530

    IMG5021-Phone nmber section aded to the sign up form

--- cliend/src/pages/User/SignUp.jsx
+++ cliend/src/pages/User/SignUp.jsx
@@ -12,6 +12,7 @@ const SignUp = () => {
     firstName: '',
     lastName : '',
     address  : '',
+    number   : '',
     email    : '',
     password : '',
   });
@@ -37,6 +38,7 @@ const SignUp = () => {
         email    : formData.email,
         password : formData.password,
         address  : formData.address,
+        number  : formData.number,
       });
 
       // 2️⃣  store user + token
@@ -44,6 +46,7 @@ const SignUp = () => {
         _id       : data._id,
         name      : data.name,
         email     : data.email,
+        number  : formData.number,
         profilePic: assets.profile2,  // backend can return real pic later
         token     : data.token,
       });
@@ -51,6 +54,7 @@ const SignUp = () => {
         _id   : data._id,
         name  : data.name,
         email : data.email,
+        number  : formData.number,
         token : data.token,
       }));
       localStorage.setItem('token', data.token);
@@ -104,6 +108,20 @@ const SignUp = () => {
               name="address"
               placeholder="Address"
               value={formData.address}
+              onChange={handleChange}
+              className="w-full border px-4 py-2 rounded"
+              required
+            />
+          </div>
+
+          {/* Phone Number */}
+          <div>
+            <label className="block mb-1 font-medium">Phone Number</label>
+            <input
+              type="number"
+              name="number"
+              placeholder="Phone number"
+              value={formData.number}
               onChange={handleChange}
               className="w-full border px-4 py-2 rounded"
               required
