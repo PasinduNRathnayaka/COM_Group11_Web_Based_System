6c448a86e2dbeb1eac8e03624fc396f05bc9b5df
Pasindu-Indusara23 <pasinduindusara9@gmail.com>
2025-07-15 23:53:04 +0530

    IMG5010-login controller added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -40,3 +40,21 @@ export const registerUser = asyncHandler(async (req, res) => {
   }
 
   });
+
+  /*  Login controller placeholder */
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid credentials');
+  }
+});
