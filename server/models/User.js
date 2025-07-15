9711e01a139fee8a412ff330fd4cb308b2bacf45
Pasindu-Indusara23 <pasinduindusara9@gmail.com>
2025-07-15 11:32:03 +0530

    IMG5002-User.js file updated with some alterations for name ,email ,passwoed and profile pic

--- server/models/User.js
+++ server/models/User.js
@@ -2,9 +2,30 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-});
+  name: {
+      type: String,
+      required: [true, 'Please enter your name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please enter an email'],
+      unique: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        'Please add a valid email',
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+      minlength: 6,
+    },
+    profilePic: {
+      type: String,
+      default: '', // placeholder until user uploads
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model('User', userSchema);
