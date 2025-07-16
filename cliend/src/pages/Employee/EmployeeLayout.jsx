94b323783b52fd949684e18873ff6e0617720b13
Dewindi1 <dewindidesilva2000@gmail.com>
2025-07-16 19:20:01 +0530

    DEF0403 - change EmployeeLayout.jsx

--- cliend/src/pages/Employee/EmployeeLayout.jsx
+++ cliend/src/pages/Employee/EmployeeLayout.jsx
@@ -109,9 +109,7 @@ const EmployeeLayout = () => {
   { name: "Check Payment", path: "/employee/salary" },
   { name: "Apply For Leave", path: "/employee/leave" },
   { name: "Download ID", path: "/employee/download-id" },
-  { name: "Customer Replies", path: "/employee/replies" },
-  { name: "Order List", path: "/employee/order-list" },
-  { name: "Order Details", path: "/employee/order-details" },
+ 
   { name: "Back", path: "/employee" } // assuming this is the target for the BACK button
 ];
 
