dd321065ad59633ca2241f13577834c5c81cfa1b
Pasindu-Indusara23 <pasinduindusara9@gmail.com>
2025-07-16 19:24:12 +0530

    IMG5028-authmiddleware file created for login validation

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,1 +1,23 @@
 // Token checking, role validation
+import jwt from 'jsonwebtoken';
+
+export const protect = (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded; // Or fetch user data here if needed
+      next();
+    } catch (error) {
+      res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  } else {
+    res.status(401).json({ message: 'Not authorized, no token' });
+  }
+};
+
