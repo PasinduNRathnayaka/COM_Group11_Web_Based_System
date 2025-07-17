c4c6a0ae52b3ed22e2306646b6b35d5f9b488ed2
Pasindu-Indusara23 <pasinduindusara9@gmail.com>
2025-07-17 13:51:46 +0530

    IMG6007-signpu.jsx updated

--- cliend/src/pages/User/SignUp.jsx
+++ cliend/src/pages/User/SignUp.jsx
@@ -47,6 +47,7 @@ const SignUp = () => {
         name      : data.name,
         email     : data.email,
         number  : formData.number,
+        address: formData.address,
         profilePic: assets.profile2,  // backend can return real pic later
         token     : data.token,
       });
@@ -55,6 +56,7 @@ const SignUp = () => {
         name  : data.name,
         email : data.email,
         number  : formData.number,
+        address: formData.address,
         token : data.token,
       }));
       localStorage.setItem('token', data.token);
@@ -100,20 +102,6 @@ const SignUp = () => {
             </div>
           </div>
 
-          {/* Address */}
-          <div>
-            <label className="block mb-1 font-medium">Address</label>
-            <input
-              type="text"
-              name="address"
-              placeholder="Address"
-              value={formData.address}
-              onChange={handleChange}
-              className="w-full border px-4 py-2 rounded"
-              required
-            />
-          </div>
-
           {/* Phone Number */}
           <div>
             <label className="block mb-1 font-medium">Phone Number</label>
@@ -122,6 +110,20 @@ const SignUp = () => {
               name="number"
               placeholder="Phone number"
               value={formData.number}
+              onChange={handleChange}
+              className="w-full border px-4 py-2 rounded"
+              required
+            />
+          </div>
+
+           {/* Address */}
+          <div>
+            <label className="block mb-1 font-medium">Address</label>
+            <input
+              type="string"
+              name="address"
+              placeholder="Address"
+              value={formData.address}
               onChange={handleChange}
               className="w-full border px-4 py-2 rounded"
               required
