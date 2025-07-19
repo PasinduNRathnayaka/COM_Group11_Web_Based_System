2294bb4b977de9ad2c14ed7ea1d3253990f98e2f
Pasindu Niroshan Rathnayaka <rathnayaka12106@usci.ruh.ac.lk>
2025-07-19 17:17:27 +0530

    PQR2110 - update it for edit employee

--- server/routes/Seller/employee.routes.js
+++ server/routes/Seller/employee.routes.js
@@ -3,13 +3,19 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import { createEmployee, getEmployees } from '../../controllers/Seller/employeeController.js';
+import {
+  createEmployee,
+  getEmployees,
+  getEmployeeById,
+  updateEmployee,
+} from '../../controllers/Seller/employeeController.js';
 
 const router = express.Router();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const employeeUploadDir = path.resolve(__dirname, '../../uploads/employees');
+
 if (!fs.existsSync(employeeUploadDir)) {
   fs.mkdirSync(employeeUploadDir, { recursive: true });
 }
@@ -18,9 +24,13 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, employeeUploadDir),
   filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname),
 });
+
 const upload = multer({ storage });
 
+// Routes
 router.post('/', upload.single('image'), createEmployee);
 router.get('/', getEmployees);
+router.get('/:id', getEmployeeById);
+router.put('/:id', upload.single('image'), updateEmployee);
 
 export default router;
