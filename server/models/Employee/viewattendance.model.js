473b06dc6345f8ed2b3df25f249747dd131a6b1b
TharuApk99 <tharushirajapaksha11@gmail.com>
2025-07-23 22:29:09 +0530

    THA0713-create viewattendance.model.js for viewattendance backend

---
+++ server/models/Employee/viewattendance.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const attendanceSchema = new mongoose.Schema({
+  employee: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Employee',
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  checkIn: {
+    type: String,
+    required: true,
+  },
+  checkOut: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+const ViewAttendance = mongoose.model('ViewAttendance', attendanceSchema);
+export default ViewAttendance;
