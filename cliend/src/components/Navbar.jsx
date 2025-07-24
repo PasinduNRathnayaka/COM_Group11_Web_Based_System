2fe5c4f34c11a17fa0900b5554bd851e0dd8dba6
Pasindu-Indusara23 <pasinduindusara9@gmail.com>
2025-07-24 18:06:45 +0530

    IMG11039-Nav bar updated

--- cliend/src/components/Navbar.jsx
+++ cliend/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
       </NavLink>
 
       {/* ✅ Desktop Menu */}
-      <div className="hidden sm:flex flex-1 justify-center gap-12 text-white font-medium">
+      <div className="hidden sm:flex flex-1 justify-center gap-12 text-white font-medium pl-100">
         <NavLink to='/'>Home</NavLink>
         <Link to="/allproducts" className="font-medium">
           All Products
